2a096245fed6a11f69c5bf1f0728c857a5d905a3
sunshuwei <1070980927@qq.com>
2019-03-18 16:24:08 +0800

    Signed-off-by: sunshuwei <1070980927@qq.com>

--- tools/src/main/java/com/example/tools/testDateTimeExamples.java
+++ tools/src/main/java/com/example/tools/testDateTimeExamples.java
@@ -27,11 +27,11 @@ public class testDateTimeExamples {
         log("两个时间之间的年数："+Moment.getDateRangeYearNum(LocalDateTime.now().plusDays(-500).toLocalDate(),LocalDateTime.now().toLocalDate()).toString());
         log("==============================================================================");
         GLatLngEntity n1 = new GLatLngEntity();
-        n1.setLongitude(BigDecimal.valueOf(116.402212));
-        n1.setLatitude(BigDecimal.valueOf(39.913697));
+        n1.setLongitude(BigDecimal.valueOf(116.40576));
+        n1.setLatitude(BigDecimal.valueOf(39.913825));
         GLatLngEntity n2 = new GLatLngEntity();
-        n2.setLongitude(BigDecimal.valueOf(116.405764));
-        n2.setLatitude(BigDecimal.valueOf(39.913825));
+        n2.setLongitude(BigDecimal.valueOf(117.126277));
+        n2.setLatitude(BigDecimal.valueOf(36.660414));
         log("坐标点之间的距离："+Gis.getGLtLngDistance(n1,n2));
 
     }
