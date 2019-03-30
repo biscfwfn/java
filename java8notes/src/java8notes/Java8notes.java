934a222af0d80911105db4e36b5e2707efd72d29
sun <biscfwfn@163.com>
2019-03-30 10:28:42 +0800

    1

--- java8notes/src/java8notes/Java8notes.java
+++ java8notes/src/java8notes/Java8notes.java
@@ -1,12 +1,13 @@
 package java8notes;
 
 public class Java8notes {
-
     public static void main(String[] args) {
         boolean f = false;
         if(!f){
-            System.out.println(f);
+            log(f);
         }
     }
-    
+    private static void log(Object s){
+        System.err.println(s);
+    }
 }
