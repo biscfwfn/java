87cc721ec7b8d4dbaf9ccbcf122c1121ec9b48b4
sunshuwei <1070980927@qq.com>
2019-08-02 16:38:25 +0800

    1

--- java8notes/src/java8notes/Java8notes.java
+++ java8notes/src/java8notes/Java8notes.java
@@ -12,6 +12,9 @@ public class Java8notes {
         for(int i=1;i<11;i++){
             log(s.pop());
         }
+        log("sdfdfasdfasdfadsf");
+        String regionCode = "500000000000";
+        log(regionCode.replaceAll("(0+)$", ""));
     }
     private static void log(Object s){
         System.out.println(s);
